6c50a8a4201e1b3f69d892bb51d8e487869e4550
saqemlas <saqemlas@googlemail.com>
2019-03-11 11:57:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,17 +2,27 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
+  # Sample static route.
   get '/hello' do
     "Hello World!"
   end
 
-  # This is a sample dynamic route.
+  # Sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @n1 = Integer(params[:num1])
+    @n2 = Integer(params[:num2])
+    "<h1>#{@n1*@n2}</h1>"
+  end
+
 
 end
